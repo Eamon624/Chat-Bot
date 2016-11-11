0fe29eb9a00ecb344f73a8c9eb5029144b875968
Eamon624 <Eamon624@hotmail.com>
2016-11-11 15:20:17 +0000

    Changing Server name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server');
+    res.send('Facebook ChatBot Server for NCI');
 });
 
 // Facebook Webhook
