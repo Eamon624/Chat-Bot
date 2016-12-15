c555a9ebcfbc652abd9c3f35d51849c98320c636
Eamon624 <Eamon624@hotmail.com>
2016-12-15 11:48:45 +0000

    ahesjfgkfsdjh

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ function listMoodle(id)
                 "buttons":[
                     {
                         "type":"web_url",
-                        "url":"https://moodle.itb.ie/login/index.php",
+                        "url":"https://moodle.ncirl.ie/",
                         "title":"Moodle"
                     },
                     {
