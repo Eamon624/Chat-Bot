4cd6bcffa1cecc9df1edced14e01fa00e6312b92
Eamon624 <Eamon624@hotmail.com>
2017-01-25 21:39:18 +0000

    Sample Chat Bot

--- index.js
+++ index.js
@@ -650,6 +650,118 @@ var TaraTemplate = {
   sendMessage(id, TaraTemplate);
 }
 
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message:
+    {
+      "attachment": {
+        "type": "template",
+        "payload": {
+         "template_type": "generic",
+          "elements": [
+          {
+            "title": "Bots",
+            "subtitle": "The rise of the Facebook Bot!",
+            "item_url": "http://www.dynamic-memory.com/",
+            "image_url": "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/img/robot.png",
+            "buttons": [
+            {
+              "type": "postback",
+              "title": "What is this Bot?",
+              "payload": "What is this Robot?"
+            },
+            {
+              "type": "postback",
+              "title": "Your Business Bot",
+              "payload": "business"
+            },
+            {
+              "type": "postback",
+              "title": "I want a Bot!",
+              "payload": "I want one"
+            }
+            ]
+          },
+          {
+            "title": "DMS Software",
+            "subtitle": "Software Engineering is awesome",
+            "item_url": "http://www.dynamic-memory.com/",
+            "image_url": "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/img/evolution.png",
+            "buttons": [
+            {
+              "type": "postback",
+              "title": "Contact",
+              "payload": "Contact"
+            },
+            {
+              "type": "postback",
+              "title": "Social media",
+              "payload": "Social media"
+            },
+            {
+              "type": "postback",
+              "title": "Matthew's bio",
+              "payload": "bio"
+            }
+            ]
+          },
+          {
+            "title": "Custom Examples",
+            "subtitle": "A few small apps to give an idea of the possibilites",
+            "item_url": "https://dynamic-memory.com",
+            "image_url": "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/img/danger-man-at-work-hi.png",
+            "buttons": [
+            {
+              "type": "postback",
+              "title": "Tell me a joke 😜",
+              "payload": "joke"
+            },
+            {
+              "type": "postback",
+              "title": "Random Image",
+              "payload": "image"
+            },
+            {
+              "type": "postback",
+              "title": "Who am I?",
+              "payload": "who"
+            }
+            ]
+          },
+          {
+            "title": "Bot Examples",
+            "subtitle": "Some great bots",
+            "item_url": "https://developers.facebook.com/products/messenger/",
+            "image_url": "https://raw.githubusercontent.com/matthewericfisher/fb-robot/master/img/example.jpeg",
+            "buttons": [
+            {
+              "type": "web_url",
+              "url": "https://www.messenger.com/t/HealthTap",
+              "title": "Health Tap"
+            },
+            {
+              "type": "web_url",
+              "url": "http://www.messenger.com/t/EstherBot",
+              "title": "Esther's cool bot"
+            },
+            {
+              "type": "web_url",
+              "url": "http://www.messenger.com/t/techcrunch",
+              "title": "TechCrunch news bot"
+            }
+            ]
+          }
+          ]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+}
 function sendCustomMessage(recipientId,messageText) {
 
 console.log("sendCustoMessage "+ messageText);
