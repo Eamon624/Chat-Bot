4ee8539e349c3048d23b30e6098f33296229ea9d
Eamon624 <Eamon624@hotmail.com>
2017-01-26 15:05:59 +0000

    uighsdfohsje

--- index.js
+++ index.js
@@ -707,58 +707,6 @@ function IrishRail(Stationfullname){
  }
 
 
- /**** Dublin bus API ******/
- function dublinBus(stopId){
-     //url is set with the bus stop number passed by the event.message
-     var options = {
-         url: 'http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+stopId+'&format=json',
-         method : 'GET'
-     };
-     //Request is made using the options and callback functions
-     request(options, callback);
-  }
-
- let message = "";
- function callback(error, response, body) {
-         body = JSON.parse(body);
-         //numberofresults will return as 0 if it past half 11
-         if(body.numberofresults === 0){
-             message = "Nope";
-         }
-         else{
-             var resultCount = 0;
-             //Display all the bus routes and due times available
-             for( var i in body.results){
-                 if(body.results[i].route == busNumber || all == true){
-                     //If the bus is due now, dont display "due in due minutes"
-                     if(body.results[i].duetime === "Due"){
-                         message += body.results[i].route + " to " + body.results[i].destination + " due now\n";
-                     }
-                     //Stop 1 minute appearing as "1 minutes"
-                     else if(body.results[i].duetime === "1"){
-                         message += body.results[i].route + " to " + body.results[i].destination + " due in " + body.results[i].duetime
-                         + " minute\n";
-                     }
-                     else{
-                         message += body.results[i].route + " to " + body.results[i].destination + " due in " + body.results[i].duetime
-                         + " minutes\n";
-                     }
-                     resultCount++;
-                 }
-             }
-             //Check if there is not times available
-             if(resultCount === 0){
-                 message = "There is currently no times available for " + busNumber + "";
-             }
-         }
-         // reset the message variable back to null to prevent double values
-         all = false;
-         busNumber = "";
-         sendMessage(recipientId, {text: message});
-         message = "";
- }
-
-
 /*********************** PICTURE RESPONSES **************************** */
 
 /**
