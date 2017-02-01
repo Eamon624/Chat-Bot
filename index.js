05c69aca85e5e98ff87cb6ac5f3eebb8d1393a52
Eamon624 <Eamon624@hotmail.com>
2017-02-01 14:16:29 +0000

    The Bus

--- index.js
+++ index.js
@@ -707,16 +707,16 @@ function callback(error, response, body) {
                 if(body.results[i].route == busNumber || all == true){
                     //If the bus is due now, dont display "due in due minutes"
                     if(body.results[i].duetime === "Due"){
-                        message += "The" + body.results[i].route + " to " + body.results[i].destination + " is due now\n\n" + " \n" ;
+                        message += "The" + body.results[i].route + " to " + body.results[i].destination + " is due now\n" + " \n" ;
                     }
                     //Stop 1 minute appearing as "1 minutes"
                     else if(body.results[i].duetime === "1"){
                         message += "The " + body.results[i].route + " to " + body.results[i].destination + "bis due in " + body.results[i].duetime
-                        + " minute\n\n" +  " \n";
+                        + " minute\n" +  " \n";
                     }
                     else{
                         message += "The " + body.results[i].route + " to " + body.results[i].destination + " is due in " + body.results[i].duetime
-                        + " minutes\n\n" + " \n" ;
+                        + " minutes\n" + " \n" ;
                     }
                     resultCount++;
                 }
