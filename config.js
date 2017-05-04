0f31fcd57e9f5956fa019522424d54e465516784
Eamon624 <Eamon624@hotmail.com>
2017-05-04 15:38:33 +0100

    Api.ai Starting Point

--- config.js
+++ config.js
@@ -1,13 +1,7 @@
-/**
- * These are used by the app of authentication.
- *
-
- *
- */
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAJ2aBX63yMBAPmke2JGBlZBFpADDdgVwcdrlDH2nthuxfs3ZAjOHVZAAHUjvadzOC9io7f7siZAzua5Ji8VVPHGqukoegD9gmpdqs3xm6bUZCxIoThXyVeMmZBWu8KIvHtTFtqdcndzmUxCW2YAtsZCOp36ZCLVvhdEZBJqHLpfBDQZDZD',
-    FB_VERIFY_TOKEN: 'secret',
-    FB_APP_SECRET: '8e3c62def3af61939a9c4ba4628517f3',
-    API_AI_CLIENT_ACCESS_TOKEN: '5c09189e70614b58a8ad579c364bbf3b',
+    FB_PAGE_TOKEN: 'EAAJ2aBX63yMBADtHALl6LFQZAw5cR6IpwThigahOYx8m2DqEkdHnsYBVi0fmrhQdJpswOgUbm45Gwu9ZBmoG3rjKOFEZAzd82G9bZCNDJgZAIrZCSxZCfcVZBKGMiHNfPmQj2BppZCmxJQjgUGBYhkLDLn69MqZBw3hWT0sWr0kMqYNgZDZD',
+    FB_VERIFY_TOKEN: 'testbot_verify_token',
+    API_AI_CLIENT_ACCESS_TOKEN: '6c88a0aa9da740fca6199eac4f21f061',
+    FB_APP_SECRET: '40db0f0c6967118338c94cb89288925e',
     SERVER_URL: "https://fathomless-atoll-13950.herokuapp.com/",
 };
