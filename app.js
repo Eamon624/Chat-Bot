e8918718880baba3242e8f71a48cc454e0b4f3e1
Eamon624 <Eamon624@hotmail.com>
2017-05-06 18:02:59 +0100

    Submit User Name to Database

--- app.js
+++ app.js
@@ -679,66 +679,36 @@ function sendAccountLinking(recipientId) {
 
 
 function greetUserText(userId) {
-    request({
-        uri: 'https://graph.facebook.com/v2.7/' + userId,
-        qs: {
-            access_token: config.FB_PAGE_TOKEN
-        }
+	//first read user firstname
+	request({
+		uri: 'https://graph.facebook.com/v2.7/' + userId,
+		qs: {
+			access_token: config.FB_PAGE_TOKEN
+		}
 
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
 
-            var user = JSON.parse(body);
+			var user = JSON.parse(body);
+			console.log("getUserData" + user);
 
-            if (user.first_name) {
-                console.log("FB user: %s %s, %s",
-                    user.first_name, user.last_name, user.gender);
+			if (user.first_name) {
+				console.log("FB user: %s %s, %s",
+					user.first_name, user.last_name, user.gender);
 
+				sendTextMessage(userId, "Hi " + user.first_name + '! Ask me something about the NCI or lets just have a chat!');
 
-                pg.connect(process.env.DATABASE_URL, function (err, client) {
-                    if (err) throw err;
-                    console.log('connect to postgres. Searching for a user...');
-                    let rows = [];
-                    client
-                        .query(`SELECT id FROM users WHERE fb_id = '${userId}' LIMIT 1`)
-                        .on('row', function (row) {
-                            rows.push(row);
-                        })
-                        .on('end', () => {
-                            if (rows.length === 0 ) {
-                                let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-                                    'locale, timezone, gender)' +
-                                    'VALUES ($1, $2, $3, $4, $5, $6, $7)';
+				
+			} else {
+				console.log("Cannot get data for fb user with id",
+					userId);
+			}
+		} else {
+			console.error(response.error);
+		}
 
-                                client.query(sql, [
-                                    userId,
-                                    user.first_name,
-                                    user.last_name,
-                                    user.profile_pic,
-                                    user.locale,
-                                    user.timezone,
-                                    user.gender
-                                ])
-                            }
-                        })
-
-                } )
-
-
-            } else {
-                console.log("Cannot get data for fb user with id",
-                    userId);
-            }
-        } else {
-            console.error(response.error);
-        }
-
-    });
+	});
 }
-
-
-
-
 
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll
