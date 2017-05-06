34f35b498cf89399e286b8be652fdae44dfef65f
Eamon624 <Eamon624@hotmail.com>
2017-05-06 18:24:23 +0100

    Send User Name to Database

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const uuid = require('uuid');
 const userData = require('./user');
 
-pg.defauls.ssl = true;
+pg.defaults.ssl = true;
 
 
 // Messenger API parameters
@@ -679,36 +679,62 @@ function sendAccountLinking(recipientId) {
 }
 
 
-function greetUserText(userId) {
-	//first read user firstname
-	request({
-		uri: 'https://graph.facebook.com/v2.7/' + userId,
-		qs: {
-			access_token: config.FB_PAGE_TOKEN
-		}
+function greetUserText(userId){
+    request({
+        uri: 'https://graph.facebook.com/v2.7/' + userId,
+        qs: {
+            access_token: config.FB_PAGE_TOKEN
+        }
 
-	}, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-			var user = JSON.parse(body);
-			console.log("getUserData" + user);
+            var user = JSON.parse(body);
 
-			if (user.first_name) {
-				console.log("FB user: %s %s, %s",
-					user.first_name, user.last_name, user.gender);
-
-				sendTextMessage(userId, "Hi " + user.first_name + '! Ask me something about the NCI or lets just have a chat!');
+            if (user.first_name) {
+                console.log("FB user: %s %s, %s",
+                    user.first_name, user.last_name, user.gender);
 
 
-			} else {
-				console.log("Cannot get data for fb user with id",
-					userId);
-			}
-		} else {
-			console.error(response.error);
-		}
+                pg.connect(process.env.DATABASE_URL, function (err, client) {
+                    if (err) throw err;
+                    console.log('connect to postgres. Searching for a user...');
+                    let rows = [];
+                    client
+                        .query(`SELECT id FROM users WHERE fb_id = '${userId}' LIMIT 1`)
+                        .on('row', function (row) {
+                            rows.push(row);
+                        })
+                        .on('end', () => {
+                            if (rows.length === 0 ) {
+                                let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
+                                    'locale, timezone, gender)' +
+                                    'VALUES ($1, $2, $3, $4, $5, $6, $7)';
 
-	});
+                                client.query(sql, [
+                                    userId,
+                                    user.first_name,
+                                    user.last_name,
+                                    user.profile_pic,
+                                    user.locale,
+                                    user.timezone,
+                                    user.gender
+                                ])
+                            }
+                        })
+
+                } )
+                
+
+            } else {
+                console.log("Cannot get data for fb user with id",
+                    userId);
+            }
+        } else {
+            console.error(response.error);
+        }
+
+    });
 }
 
 /*
