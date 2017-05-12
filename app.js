b64cec74ff2a9fb170482b9d861fa6a3169c826c
Eamon624 <Eamon624@hotmail.com>
2017-05-12 15:00:38 +0100

    Sangrid Added

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 					&& job_vacancy != '')
 
 					{
-					let emailContent = 'Feeback recieved from ' + user_name + '<br> <br>'
+					let emailContent = 'Feeback recieved from ' + user_name + '.<br> <br>'
 
 						'.<br> Student Number: ' + student_number + '.' +
 						'.<br> Conatct Number: ' + contact_number + '.' +
@@ -314,55 +314,6 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 
 							break;
 
-							case "Give-Feedback":
-									if (isDefined(contexts[0]) &&
-										(contexts[0].name == 'job_application' || contexts[0].name == 'job-application-details_dialog_context')
-										&& contexts[0].parameters) {
-										let phone_number = (isDefined(contexts[0].parameters['phone-number'])
-										&& contexts[0].parameters['phone-number']!= '') ? contexts[0].parameters['phone-number'] : '';
-										let user_name = (isDefined(contexts[0].parameters['user-name'])
-										&& contexts[0].parameters['user-name']!= '') ? contexts[0].parameters['user-name'] : '';
-										let previous_job = (isDefined(contexts[0].parameters['previous-job'])
-										&& contexts[0].parameters['previous-job']!= '') ? contexts[0].parameters['previous-job'] : '';
-										let years_of_experience = (isDefined(contexts[0].parameters['years-of-experience'])
-										&& contexts[0].parameters['years-of-experience']!= '') ? contexts[0].parameters['years-of-experience'] : '';
-										let job_vacancy = (isDefined(contexts[0].parameters['job-vacancy'])
-										&& contexts[0].parameters['job-vacancy']!= '') ? contexts[0].parameters['job-vacancy'] : '';
-
-										if (phone_number == '' && user_name != '' && previous_job != '' && years_of_experience == '') {
-											let replies = [
-												{
-													"content_type":"text",
-													"title":"Less than 1 year",
-													"payload":"Less than 1 year"
-												},
-												{
-													"content_type":"text",
-													"title":"Less than 10 year",
-													"payload":"Less than 10 year"
-												},
-												{
-													"content_type":"text",
-													"title":"More than 10 years",
-													"payload":"More than 10 years"
-												}
-											];
-											sendQuickReply(sender, responseText, replies);
-										} else if (phone_number != '' && user_name != '' && previous_job != '' && years_of_experience != ''
-											&& job_vacancy != '') {
-											let emailContent = 'A new job enquiery from ' + user_name + ' for the job: ' + job_vacancy +
-												'.<br> Previous job position: ' + previous_job + '.' +
-												'.<br> Years of experience: ' + years_of_experience + '.' +
-												'.<br> Phone number: ' + phone_number + '.';
-
-											sendEmail('New job application', emailContent);
-											sendTextMessage(sender, responseText);
-										} else {
-											sendTextMessage(sender, responseText);
-										}
-									}
-
-									break;
 
 		default:
 			//unhandled action, just send back the text
