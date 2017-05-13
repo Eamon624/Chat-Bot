606fb279f9682ce6032a4a3646c67a5e5189f5a8
Eamon624 <Eamon624@hotmail.com>
2017-05-13 18:00:35 +0100

    Email Function

--- app.js
+++ app.js
@@ -216,10 +216,17 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 				 let feed_back = (isDefined(contexts[0].parameters['feedback']) && contexts[0].parameters['feedback']!= '') ? contexts[0].parameters['feedback']: '';
 
 				 if (phone_number != '' && user_name != '' && previous_job != '' && years_of_experience != '' && job_vacancy != '' ){
-          let emailContent = "A new job enquiry from"  + user_name + " for the job: " + job_vacancy +
-                            ".<br> Student Number: " + previous_job + "." +
-                            ".<br> Year: " + years_of_experience + "." +
-                            ".<br> <br> Feedback: " + phone_number + ". ";
+          let emailContent = "Name: "  + user_name + "."
+
+														"<br> <br>"
+														"Topic:" + job_vacancy + "."
+
+                            "<br> <br>"
+
+														"Student Number: " + previous_job + "." +
+                            "<br> Year: " + years_of_experience + "." +
+														
+                            "<br> <br> Feedback: " + phone_number + ". ";
 
           sendEmail('New Feedback Submission', emailContent);
 
